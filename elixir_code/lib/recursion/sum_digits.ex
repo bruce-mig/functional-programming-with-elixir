4dc0aca55bd52150f319fa1ec0e80b0809bce2d3
bruce-mig <brucetmig@icloud.com>
2024-12-27 21:58:22 +0200

    tail recursion

--- elixir_code/lib/recursion/sum_digits.ex
+++ elixir_code/lib/recursion/sum_digits.ex
@@ -2,6 +2,13 @@ defmodule ElixirCode.Recursion.SumDigits do
   def upto(0), do: 0
 
   def upto(num) do
-      num + upto(num - 1)
+      upto_tail_rec(num)
+  end
+
+  def upto_tail_rec(num, acc \\ 0) # Function definition
+  def upto_tail_rec(0, acc), do: acc # Base condition
+
+  def upto_tail_rec(num, acc) do
+    upto_tail_rec(num - 1, acc + num)
   end
 end
