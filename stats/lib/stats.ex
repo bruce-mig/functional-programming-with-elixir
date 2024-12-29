a0b60926e378e3d91494817e02c7e9eb7191a432
bruce-mig <brucetmig@icloud.com>
2024-12-29 09:04:13 +0200

    median

--- stats/lib/stats.ex
+++ stats/lib/stats.ex
@@ -1,8 +1,10 @@
 defmodule Stats do
-  alias Stats.CentralTendency.Mean
+  alias Stats.CentralTendency.{Mean, Median}
 
   # def population_mean(nums), do: Mean.population_mean(nums)
   defdelegate population_mean(nums), to: Mean
   defdelegate sample_mean(nums), to: Mean
+
+  defdelegate median(nums_list), to: Median
 
 end
