196d5f30b2eb3a03a1e542914fbb3560de14d990
bruce-mig <brucetmig@icloud.com>
2024-12-29 07:09:55 +0200

    structs

--- elixir_code/lib/structs/seven_wonders.ex
+++ elixir_code/lib/structs/seven_wonders.ex
@@ -36,6 +36,33 @@ defmodule ElixirCode.Structs.SevenWonders do
     |> Enum.filter(fn %{country: country_name} -> country_name == country end)
   end
 
+  @spec in_countries_starting_with_i([t()]) :: [t()]
+  def in_countries_starting_with_i(wonders) do
+    wonders
+    |> Enum.filter(fn %{country: country} ->  String.starts_with?(country, "I") end)
+  end
+
+  @spec sort_by_country_length([t()]) :: [t()]
+  def sort_by_country_length(wonders) do
+    wonders
+    |> Enum.sort(fn x,y -> String.length(x.country) < String.length(y.country) end)
+  end
+
+  def name_country_list(wonders) do
+    wonders
+    |> Enum.reduce([], fn wonder, acc -> [[wonder.name, wonder.country] | acc] end)
+  end
+
+  def country_name_keyword_list(wonders) do
+    wonders
+    |> Enum.reduce([], fn wonder, acc -> [{String.to_atom(wonder.name), wonder.country} | acc] end)
+  end
+
+  # ------------------------------------- For -------------------------------------
+
+  def all_names(wonders) do
+    for %{name: name} <- wonders, do: name
+  end
 
 
 end
